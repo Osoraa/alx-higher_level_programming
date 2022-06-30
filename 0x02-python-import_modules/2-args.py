e92f1dddd86ee55329aec0dddd2335b3613f0227
Osoraa <sochi98.sn@gmail.com>
2022-06-30 20:45:31 +0100

    Task 2 - How to make a script dynamic!

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,7 +8,7 @@ def main():
         print("0 arguments.")
         return
 
-    print("{:d} argument{:s}".format(argLen, "s" if argLen > 1 else ""))
+    print("{:d} argument{:s}:".format(argLen, "s" if argLen > 1 else ""))
 
     for index in range(1, argLen + 1):
         print(f"{index:d}: {argv[index]:s}")
