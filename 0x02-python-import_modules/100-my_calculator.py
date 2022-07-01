88a195d0e6e38c023287f401cc9edeb87cfbb237
Osoraa <sochi98.sn@gmail.com>
2022-07-01 02:45:10 +0100

    Task 6 - Build my own calculator!

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+from sys import exit, argv
+from calculator_1 import add, sub, div, mul
+
+
+def main():
+    if len(argv) != 4:
+        # print(f"Usage: {argv[0]:s} <a> <operator> <b>")
+        for arg in argv:
+            print(arg)
+        exit(1)
+
+    a, op, b = int(argv[1]), argv[2], int(argv[3])
+    op_dict = {
+        "+": add(a, b),
+        "-": sub(a, b),
+        "*": mul(a, b),
+        "/": div(a, b),
+    }
+
+    if op not in "+-*/":
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
+    print("{} {} {} = {}".format(a, op, b, op_dict[op]))
+
+
+if __name__ == "__main__":
+    main()
