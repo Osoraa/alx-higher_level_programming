17c4f357e3eb3c12e5612f83df52b31fb6c6bad3
Osoraa <sochi98.sn@gmail.com>
2022-07-01 01:48:26 +0100

    Task 4 - Who are you?

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,7 +3,7 @@ import hidden_4
 
 
 def main():
-    names = [name for name in dir("") if name[:2] != "__"]
+    names = [name for name in dir(hidden_4) if name[:2] != "__"]
     for name in names:
         print(name)
 
