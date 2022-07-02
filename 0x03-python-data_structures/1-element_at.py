88cdda50efedaabc921c6c3f2ba4a98a83e41ac6
Osoraa <sochi98.sn@gmail.com>
2022-07-02 11:07:37 -0700

    Task 1 - Secure access to an element in a list

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def element_at(my_list, idx):
+    if 0 > idx >= len(list):
+        return None
+
+    return my_list[idx]
+
+
+if __name__ == "__main__":
+    element_at()
