6c00fc35b76b6be1610eb2a03d84699afe31b1d6
Osoraa <sochi98.sn@gmail.com>
2022-07-06 16:37:27 +0100

    Task 4 - Only differents

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -2,7 +2,7 @@
 
 
 def only_diff_elements(set_1=None, set_2=None) -> set:
-    return set_1.symmetric_difference(set_2) if set_1 or set_2 else None
+    return set_1.symmetric_difference(set_2) if set_1 or set_2 else ()
 
 
 if __name__ == "__main__":
