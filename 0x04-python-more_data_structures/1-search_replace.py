b698af11bbc9c839f8a1ee74dbd967f3bb9162c0
Osoraa <sochi98.sn@gmail.com>
2022-07-06 16:05:19 +0100

    Task 1 - Search and replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def search_replace(my_list=[], old=None, new=None):
+    new_list = my_list[:]
+
+    for index, value in enumerate(new_list):
+        new_list[index] = new if value is old else value
+
+    return new_list
+
+
+if __name__ == "__main__":
+    search_replace()
