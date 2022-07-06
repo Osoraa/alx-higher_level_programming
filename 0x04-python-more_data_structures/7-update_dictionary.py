961543c3bf50826bb55a6b9bea73ae5d203b641e
Osoraa <sochi98.sn@gmail.com>
2022-07-06 21:24:26 +0100

    Task 8 - Simple delete by key

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
 
-def update_dictionary(a_dict, key, value):
+def update_dictionary(a_dict=None, key="", value=None) -> dict:
     a_dict[key] = value
     return a_dict
+
+
+if __name__ == "__main__":
+    update_dictionary()
---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def simple_delete(a_dict=None, key="") -> dict:
+    a_dict.pop(key, None) # if key in a_dict.keys() else None
+    return a_dict
+
+
+if __name__ == "__main__":
+    simple_delete()
