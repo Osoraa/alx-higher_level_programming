4367c9760ad6df526f7cab42d3057d7458711dc6
Osoraa <sochi98.sn@gmail.com>
2022-07-07 00:39:53 +0100

    Task 10 - Best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -5,6 +5,8 @@ def best_score(a_dict={}):
     if not a_dict:
         return None
 
+    # Create a 2D list to hold dict key-value pair.
+    # Sort 2D list to find largest dict_value and then return associated key
     dict_list = [[key, value] for key, value in a_dict.items()]
     dict_list.sort(key=lambda x: x[1], reverse=True)
 
