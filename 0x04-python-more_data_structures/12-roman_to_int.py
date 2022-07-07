24edcfe489f5b276790bbe24720c4a6fd52cea3e
Osoraa <sochi98.sn@gmail.com>
2022-07-07 09:24:40 +0100

    Task 12 - Roman to Integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 
 
-def roman_to_int(roman_str) -> int:
+def roman_to_int(roman_str=None) -> int:
+    if roman_str is None or type(roman_str) is not str:
+        return 0
+
     r_nums = {
         "I": 1,
         "V": 5,
@@ -12,9 +15,13 @@ def roman_to_int(roman_str) -> int:
         "M": 1000
     }
 
+    # Create and reverse a list of the roman strings
     r_list = list(roman_str)[::-1]
+
     result = r_nums[r_list[0]]
 
+    # Subtract value from result if previous value is less than value
+    # else add value to result
     for i in range(1, len(r_list)):
         if r_nums[r_list[i]] < r_nums[r_list[i-1]]:
             result -= r_nums[r_list[i]]
