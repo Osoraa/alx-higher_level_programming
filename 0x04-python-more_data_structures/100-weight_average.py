9e1b1870e80e28fe7b1af783dda239199acb8864
Osoraa <sochi98.sn@gmail.com>
2022-07-07 11:31:29 +0100

    Task 13 - Weighted average!

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+
+    product = sum([num[0] * num[1] for num in my_list])
+    total_count = sum([num[1] for num in my_list])
+
+    return product / total_count
+
+
+if __name__ == "__main__":
+    weight_average()
