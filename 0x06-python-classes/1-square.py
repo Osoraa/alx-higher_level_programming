6e60968b8d306b61891ae844d93397fced61fee3
Osoraa <sochi98.sn@gmail.com>
2022-07-20 04:31:09 +0100

    Task 1 - Square with size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -8,11 +8,3 @@ class Square():
 
     def __init__(self, size=None):
         self.__size = size
-
-    # @property
-    # def _size(self):
-    #     return self.__size
-
-    # @_size.setter
-    # def _size(self, size):
-    #     self.__size = size
