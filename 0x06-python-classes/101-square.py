1c25eab4f81e7ece2a9de19abd41f8ba8cb076c1
Osoraa <sochi98.sn@gmail.com>
2022-07-21 23:33:36 +0100

    Task 8 - Print square instance

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -22,9 +22,10 @@ class Square:
             square += "\n"
 
         # Print square with each line preceeded by (space * position[0])
-        for _ in range(self.__size):
-            square += f"{' ' * self.__position[0]}{'#' * self.__size}\n"
-        
+        for unit in range(self.__size):
+            square += f"{' ' * self.__position[0]}{'#' * self.__size}"
+            square += "\n" if unit+1 < self.__size else ""
+
         return square
 
     @property
@@ -53,7 +54,6 @@ class Square:
         ):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
-
 
     def area(self):
         """Calculates the area of a square"""
