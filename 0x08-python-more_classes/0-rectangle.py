36be5abbfb75bbdccc8b07db15841b446f2adc39
Osoraa <sochi98.sn@gmail.com>
2022-07-25 17:29:23 +0100

    Task 0 - Simple rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""The Rectangle module
+
+Makes available a Rectangle class for rectangle representation
+"""
+
+
+class Rectangle:
+    """Creates an instance of a Rectangle.
+
+    Args:
+        None
+
+    Attributes:
+        Rectangle Object/Instance
+    """
