87111eabd5c5f6802baf0d02aaeacd6321f678e4
Osoraa <sochi98.sn@gmail.com>
2022-07-27 20:42:02 +0100

    Task 5 - Max integer - Unittest

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -2,13 +2,28 @@
 """Test module for the 6-max_integer module"""
 
 import unittest
-from importlib import import_module
 
-func = import_module("../6-max_integer").max_integer
+max_int = __import__("6-max_integer").max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
     """Class to test the max_integer function."""
 
-    def test_valid_arguments()
+    def test_valid_arguments(self):
+        """Valid a"""
+        self.assertEqual(max_int([1,2, 3]), 3)
+        self.assertEqual(max_int([1,2, -33]), 2)
+        self.assertEqual(max_int([1]), 1)
+        self.assertEqual(max_int([]), None)
+        self.assertEqual(max_int(), None)
+        self.assertEqual(max_int((1, 2, 3)), 3)
+        # self.assertEqual(max_int("[1, 2, 3]"), 3)
 
+    def test_invalid_arguments(self):
+        """Invalud arguments for max_int function"""
+        self.assertRaises(TypeError, max_int, [1,2, "3"])
+        self.assertRaises(TypeError, max_int, None)
+
+
+if __name__ == "__main__":
+    unittest.main()
