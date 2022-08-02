c809916ff2120027353e84eceab2546d9d3d1f61
Osoraa <sochi98.sn@gmail.com>
2022-08-02 02:59:29 +0100

    Task 1 - My list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,14 +6,20 @@ class MyList(list):
     """Extends list functionality by sorting list.
 
     Args:
-        list: List object sort and return for print.
-
-    Return:
-        Sorted list.
+        None.
     """
 
     def __init__(self):
         pass
 
     def print_sorted(self):
+        """Prints a sorted list.
+
+        Args:
+            None.
+
+        Return:
+            None.
+        """
+
         print(sorted(self))
