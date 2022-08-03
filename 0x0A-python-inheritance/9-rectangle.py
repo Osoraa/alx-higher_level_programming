c34e7b9ab87256d8d66465ba291c641386985226
Osoraa <sochi98.sn@gmail.com>
2022-08-04 00:36:02 +0100

    Task 11 - Square #2

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -13,11 +13,10 @@ class Rectangle(BaseGeometry):
     """
 
     def __init__(self, width, height):
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
 
-        self.__height = height
-        self.__width = width
+        self.__height, self.__width = height, width
 
     def __str__(self) -> str:
         return f"[{self.__class__.__name__}] {self.__width}/{self.__height}"
