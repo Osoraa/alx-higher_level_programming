d27cb3d272bd3b125aa23d12a9c04eb4d61cb777
Osoraa <sochi98.sn@gmail.com>
2022-09-10 03:55:33 +0100

    Task 4 - ID can't be null

---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,4 @@
+-- Creates a table id_not_null
+-- Adds two columns id and name
+-- id is type int with default value 1 and name is varchar(256)
+CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1, name VARCHAR(256));
