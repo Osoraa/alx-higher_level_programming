86cadba7f4a625e3d1d8f888cdf4a7d795c58157
Osoraa <sochi98.sn@gmail.com>
2022-09-10 04:26:33 +0100

    States table

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,7 +1,7 @@
 -- Creates a database
 -- Creats a table in the new database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.state
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states
 (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(256) NOT NULL
