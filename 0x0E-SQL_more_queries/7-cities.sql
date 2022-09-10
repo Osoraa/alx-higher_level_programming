3983acb1a15ab1f29a382ab84af4383366ff712a
Osoraa <sochi98.sn@gmail.com>
2022-09-10 05:40:14 +0100

    Task 7 - Cities table

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,10 @@
+-- Creates a database
+-- Creates a table with id and name columns in the new database
+-- id should be pk and should auto-increment
+-- name should non-null
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    state_id INT NOT NULL FOREIGN KEY REERENCES states(id)
+    name VARCHAR(256) NOT NULL
+);
