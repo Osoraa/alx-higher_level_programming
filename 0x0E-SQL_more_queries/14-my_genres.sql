1573396ddea38bd6d47101c5d69b2028c520ded5
Osoraa <sochi98.sn@gmail.com>
2022-09-11 02:41:02 +0100

    Task 14 - My genres

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -3,5 +3,5 @@ SELECT name
 FROM tv_genres AS tvg
 JOIN tv_show_genres ON tvg.id = genre_id
 JOIN tv_shows AS tvs ON show_id = tvs.id
-WHERE tvs.id = 8
+WHERE tvs.title = "Dexter"
 ORDER BY name ASC;
