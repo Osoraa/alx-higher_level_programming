444df21e8bbf35762305edc6bf3bb5f7da97fa1b
Osoraa <sochi98.sn@gmail.com>
2022-09-11 01:23:14 +0100

    Task 12 - No genre

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,5 +1,5 @@
 -- Lists all shows contained in a database without a linked genre
 SELECT title, genre_id
 FROM tv_shows LEFT JOIN tv_show_genres ON id = show_id
-WHERE show_id IS NULL;
-ORDER BY title, genre_id;
+WHERE show_id IS NULL
+ORDER BY title;
