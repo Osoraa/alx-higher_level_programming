147376436c9f0847e9b6175febffbe6585fae488
Osoraa <sochi98.sn@gmail.com>
2022-10-05 14:31:34 +0100

    Task 10 - Factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
-let arg = process.argv[2];
-arg = parseInt(arg);
-
 function recursion (n) {
   if (!n || n <= 1) return 1;
 
   return n * recursion(n - 1);
 }
+
+let arg = process.argv[2];
+arg = parseInt(arg);
 
 console.log(`${recursion(arg)}`);
