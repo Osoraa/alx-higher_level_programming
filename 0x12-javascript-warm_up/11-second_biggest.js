a89a681d8098b43323f8034076e713a9af0350cb
Osoraa <sochi98.sn@gmail.com>
2022-10-05 15:57:29 +0100

    Task 11 - Second biggest!

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+function filterSort (args) {
+  const nums = [];
+  for (const arg of args) {
+    if (parseInt(arg)) {
+      nums.push(parseInt(arg));
+    }
+  }
+  
+  if (nums.length <= 1) {
+    return [0, 0];
+  } else {
+    return nums.sort((a, b) => { return a - b; });
+  }
+}
+
+const result = filterSort(process.argv.slice(2));
+
+console.log(result[result.length - 2]);
