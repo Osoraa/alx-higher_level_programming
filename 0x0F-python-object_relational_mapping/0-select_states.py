fde53795c698bc4ac79f156842d4b62ce49d6073
Osoraa <sochi98.sn@gmail.com>
2022-10-06 22:00:48 +0100

    Task 0 - Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python
+""" Lists all states from the hbtn_0e_0_usa database.
+
+Usage:
+    ./0-select_states.py <user> <passwd> <database>
+
+"""
+
 from sys import argv
 import MySQLdb as mdb
 
-# Connects to and queries the hbtn_0e_0_usa database
-db = mdb.connect("localhost", argv[1], argv[2], argv[3])
-cur = db.cursor()
 
-cur.execute("SELECT * from states")
-for row in cur.fetchall():
-    print(row)
+if __name__ == "__main__":
+    db = mdb.connect("localhost", argv[1], argv[2], argv[3])
+    cur = db.cursor()
+
+    cur.execute("SELECT * from states")
+    for row in cur.fetchall():
+        print(row)
