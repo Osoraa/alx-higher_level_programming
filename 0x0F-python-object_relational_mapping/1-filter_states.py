037a04c587f7e517b1f9d3a4ae6141988090b58c
Osoraa <sochi98.sn@gmail.com>
2022-10-06 22:31:18 +0100

    Task 1 - Filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -2,6 +2,7 @@
 from sys import argv
 import MySQLdb as mdb
 
+# Selects all rows from hbtn_0e_0_usa where states.name starts with N
 cur = mdb.connect("localhost", argv[1], argv[2], argv[3]).cursor()
 
 cur.execute("SELECT id, name FROM states WHERE name LIKE 'N%'")
