feb7bff45e141878061d0154c4a98d89b64b9fc1
Osoraa <sochi98.sn@gmail.com>
2022-10-08 20:18:45 +0100

    Task 7 - All states via SQLAlchemy

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -13,7 +13,7 @@ from model_state import State
 
 
 if __name__ == "__main__":
-    db_url = f"mysql+mysqldb://{argv[1]}:{argv[2]}@localhost/{argv[3]}"
+    db_url = f"mysql://{argv[1]}:{argv[2]}@localhost/{argv[3]}"
     engine = create_engine(db_url)
     Session = sessionmaker(bind=engine)
 
