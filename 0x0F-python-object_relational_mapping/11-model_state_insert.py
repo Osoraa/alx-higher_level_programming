8778e8315dd2f14fd22a56704307f656e454edcf
Osoraa <sochi98.sn@gmail.com>
2022-10-09 09:02:37 +0100

    Task 11 - Add a new state

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Insertion script using SQLAlchemy.
+
+Inserts a single State object into a database.
+
+Example:
+    $ ./11-model_state_insert.py <db_user> <db_passwd> <database>
+
+"""
+
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State
+
+
+if __name__ == "__main__":
+    db_url = f"mysql://{argv[1]}:{argv[2]}@localhost/{argv[3]}"
+    engine = create_engine(db_url)
+    Session = sessionmaker(bind=engine)
+
+    s = State(name = "Louisiana")
+    
+    with Session.begin() as session:
+        session.add(s)
