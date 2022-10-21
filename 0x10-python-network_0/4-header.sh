8855ed81b0b7e012b46aff53bed73a21cecba3cf
Osoraa <sochi98.sn@gmail.com>
2022-10-21 13:13:14 +0100

    Task 4 - cURL headers

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Get the methods available on a url
-curl -sH "X-School-User-Id: 98" "$1" #| grep Allow | tail -c +8
+# Sets a header value and sends a request to a url
+curl -sH "X-School-User-Id: 98" "$1"
